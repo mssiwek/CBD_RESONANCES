d476bf02d2c2d2267a91a2d59d08b5f20b581cb1
Magdalena Siwek <magdalena.siwek@cfa.harvard.edu>
2024-12-02 14:17:04 -0500

    function to lighten colours

--- analysis/plotting/plot_utils.py
+++ analysis/plotting/plot_utils.py
@@ -35,14 +35,16 @@ def plot_mesh(X, Y, Z, **kwargs):
         ax = fig.add_subplot(1,1,1)
     
     v_range = 1.e-8
-    if 'v_range' in kwargs:
-        v_range = kwargs['v_range']
+    if 'vlims' in kwargs:
+        vlims = kwargs['vlims']
+    else:
+        vlims = [-1.e-8, 1.e-8]
     linthresh=1.e-10
     if 'linthresh' in kwargs:
         linthresh = kwargs['linthresh']
     
     cbar_norm_log = colors.SymLogNorm(linthresh=linthresh, linscale=0.03, \
-                                vmin=-v_range, vmax=v_range)
+                                vmin=vlims[0], vmax=vlims[1])
     
     im = ax.pcolormesh(X, Y, Z, norm=cbar_norm_log, \
                         cmap='RdBu_r')
@@ -77,3 +79,24 @@ def plot_mesh(X, Y, Z, **kwargs):
             print("No figure path or figure name given! Saving in current path as snap_interp.png")
             plt.savefig('snap_interp.png')
             plt.close()
+
+def lighten_color(color, a):
+    """
+    a: values over 1 will darken, values under 1 will lighten color
+    from: https://stackoverflow.com/questions/37765197/darken-or-lighten-a-color-in-matplotlib
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - a * (1 - c[1]), c[2])
