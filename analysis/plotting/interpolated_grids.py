5c6ccb3fd0961f19ff173d513aa7db4c3429536c
Magdalena Siwek <magdalena.siwek@cfa.harvard.edu>
2024-12-02 14:15:51 -0500

    execute interpolated_grids.py from shell directly

--- analysis/plotting/interpolated_grids.py
+++ analysis/plotting/interpolated_grids.py
@@ -16,8 +16,12 @@ linewidth = 2
 
 Pb = 2.*np.pi
 
-def plot(all_fps, figpath, titles, param, box=10, ncells = 500, BoxSize=300, \
-    dim='2D', SemiMajorAxis=1, fname='ebdot', **kwargs):
+def plot(all_fps, figpath, titles, param, \
+         vlims, \
+         cbar_symlog=True, \
+         box=10, ncells = 500, BoxSize=300, \
+         dim='2D', SemiMajorAxis=1, fname='ebdot', \
+         **kwargs):
 
     i_initial, i_final = misc.snap_inds(all_fps, kwargs)
 
@@ -41,23 +45,55 @@ def plot(all_fps, figpath, titles, param, box=10, ncells = 500, BoxSize=300, \
                 z = binevol.ebdot()
             if param == 'abdot':
                 z = binevol.abdot()
+            if param == 'rho':
+                z = snap.Snap['PartType0']['Density']
 
             #interpolate grid
             snap.interp(x_interp, y_interp, z)
             #now stack the interpolated grid onto the last one
             grid = snap.stack_grid(grid=grid)
-
-            # snap.plot(z, box=box, plot_cbar=True)
         
-        snap.plot_interp(figpath = figpath, fname = fname + '_interp_%.3d' %i)
+        snap.plot_interp(figpath = figpath, vlims=vlims, \
+                         cbar_symlog=cbar_symlog, \
+                         fname = fname + '_interp_%.3d' %i)
         title = r'$%.2f < t < %.2f$' %(t_initial/Pb, t_final/Pb)
         fname += '_stacked_grid' + '_%.2f<t<%.2f' %(t_initial/Pb, t_final/Pb)
         plot_utils.plot_mesh(X_interp, Y_interp, np.mean(grid, axis=-1), \
                              figpath = figpath, fname = fname,\
-                             title=title)
-
+                             title=title, vlims=vlims)
 
             
+if __name__ == "__main__":
+    #cbd_ebqb_study_rs_videos
+    fp_root = "/n/holylfs05/LABS/hernquist_lab/msiwek/arepo/cbd_ebqb_study_rs_videos/res=5.000e-10/alpha=0.100/dim=2/"
+    figpath = "../figures/"
+
+    param_dict = \
+        {\
+        'e': [0.8],
+        'q': [0.1],
+        'aspect_ratio': [0.10],
+        'phi': [0.0],
+        'AccretionRadius': [0.005],
+        'SinkRate': [0.50],\
+        'keys': ['e', 'q', 'aspect_ratio', 'phi', 'AccretionRadius', 'SinkRate']
+        }
+    all_fps = misc.get_fpaths_params(fp_root, param_dict, test_param = 'e')
+    titles = []
+    for eb in param_dict['e']:
+        titles.append(r'$e_{\rm b} = $' + '%.2f' %eb)
+
+    param = 'rho'
+    if param=='rho':
+        vlims=[1.e-7, 1.e-4]
+        cbar_symlog=False
+    else:
+        vlims=[-1.e-8,1.e-8]
+        cbar_symlog=True
+    fname = param + '_eb=%.2f' %param_dict['e'][0] + '_qb=%.2f' %param_dict['q'][0]
+    plot(all_fps, figpath + '/stacked_grids/', titles, param, vlims, \
+         cbar_symlog = cbar_symlog, box=10, ncells=500, BoxSize=300, \
+         SemiMajorAxis=1, fname=fname, i_initial=300, i_final=601)
 
 
             
